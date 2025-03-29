a1d943b76f5c6c581a3ece042d7089bf48be85fc
CamiloWG <millos2423@gmail.com>
2025-03-29 18:33:26 -0500

    Servicio pelicula por id

--- backend/routers/FilmRouter.py
+++ backend/routers/FilmRouter.py
@@ -10,3 +10,8 @@ router = APIRouter(prefix="/film", tags=["Peliculas"])
 @router.get("/", response_model=list[FilmSchema])
 def get_films(skip: int = 0, limit: int = 10, db: Session = Depends(db.get)):
     return db.query(Film).offset(skip).limit(limit).all()
+
+
+@router.get("/{film_id}", response_model=FilmSchema)
+def get_film_by_id(film_id: int, db: Session = Depends(db.get)):
+    return db.query(Film).filter(Film.film_id == film_id).first()
