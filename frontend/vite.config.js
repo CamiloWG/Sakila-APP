e9d708fbd50a2f08640a26af3b1d779b6b70b1f4
CamiloWG <millos2423@gmail.com>
2025-03-30 16:55:17 -0500

    Frontend

---
+++ frontend/vite.config.js
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [tailwindcss(), react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://ec2-50-19-189-124.compute-1.amazonaws.com:8000',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    },
+    host: '0.0.0.0', // O la IP específica que quieras
+    port: 80, // Puedes cambiarlo si es necesario
+    strictPort: true, // Para asegurarte de que usa ese puerto
+    allowedHosts: ['http://ec2-50-19-189-124.compute-1.amazonaws.com']
+  }
+})
